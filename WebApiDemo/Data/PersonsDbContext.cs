3644ab4ba6f61ef61feb016c1410c4d179200fbc
nkastratovic <nkastratovic@gmail.com>
2024-02-01 13:47:37 +0100

    Add DbContext.

---
+++ WebApiDemo/Data/PersonsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Data
+{
+    public class PersonsDbContext : DbContext
+    {
+        public PersonsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Person>().ToTable("Persons");
+        }
+    }
+}
