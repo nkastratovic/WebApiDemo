f380ed9d4790718598c5a12d575fbf32e873f785
nkastratovic <nkastratovic@gmail.com>
2024-02-01 13:38:31 +0100

    Add Model.

---
+++ WebApiDemo/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDemo.Models
+{
+    public class Person
+    {
+        [Key]
+        public Guid PersonId { get; set; }
+        [Required]
+        [StringLength(20)] //nvarchar(20)
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
